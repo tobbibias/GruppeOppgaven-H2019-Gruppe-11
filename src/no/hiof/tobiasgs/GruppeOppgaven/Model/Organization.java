232c6031313440bffbe861527459da85c087c8c2
Thomasly89 <56018095+Thomasly89@users.noreply.github.com>
2019-10-22 09:50:36 +0200

    editing classDiagram

--- src/no/hiof/tobiasgs/GruppeOppgaven/Model/Organization.java
+++ src/no/hiof/tobiasgs/GruppeOppgaven/Model/Organization.java
@@ -3,7 +3,7 @@ package no.hiof.tobiasgs.GruppeOppgaven.Model;
 
 import java.util.ArrayList;
 
-public interface Organization {
+public Class Organization {
     /*public String organizationName, email, postal,zipcode;
     public int organizationNr;
     public ArrayList<User> ansatte = new ArrayList<User>();
