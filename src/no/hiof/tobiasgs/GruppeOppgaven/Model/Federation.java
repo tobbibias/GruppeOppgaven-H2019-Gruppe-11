a5c05612fee047aa427807fdfcfbc0d4e7e5f312
Thomasly89 <56018095+Thomasly89@users.noreply.github.com>
2019-10-22 12:01:51 +0200

    made EventTest and EmployeeTest

--- src/no/hiof/tobiasgs/GruppeOppgaven/Model/Federation.java
+++ src/no/hiof/tobiasgs/GruppeOppgaven/Model/Federation.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 public class Federation extends Organization{
 
-
-
     private FederationEmployee federationLeader;
     private ArrayList<SportsClub> teamsInFederation = new ArrayList<SportsClub>();
     private ArrayList<Events> upCommingEvents = new ArrayList<Events>();
