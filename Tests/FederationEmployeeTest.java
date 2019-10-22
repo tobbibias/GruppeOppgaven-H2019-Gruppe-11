a5d2e6302e0e6d23ba9b1c853a733cafef0a316d
Thomasly89 <56018095+Thomasly89@users.noreply.github.com>
2019-10-22 15:46:14 +0200

    redigerte classediagram
    bytta fra norsk til engelsk

--- Tests/FederationEmployeeTest.java
+++ Tests/FederationEmployeeTest.java
@@ -1,8 +1,13 @@
+import no.hiof.tobiasgs.GruppeOppgaven.Model.Federation;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FederationEmployeeTest {
     @Test
     void fedEmployeeTest(){
-        
+        FederationEmployee fedEm = new FederationEmployee(new Federation("Norges fotball forbund",222,"norge@fotball.no","oslo","1920"),"Forbundsleder");
+        assertTrue(fedEm.getFederationName()=="Norges forball forbund");
+
     }
 }
