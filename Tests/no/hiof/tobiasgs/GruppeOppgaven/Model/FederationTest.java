a3a0349bda77d7dca2db961d324f5599dbf9bd39
Thomasly89 <56018095+Thomasly89@users.noreply.github.com>
2019-10-22 09:29:24 +0200

    editing fedTest

--- Tests/no/hiof/tobiasgs/GruppeOppgaven/Model/FederationTest.java
+++ Tests/no/hiof/tobiasgs/GruppeOppgaven/Model/FederationTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Array;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +17,7 @@ class FederationTest {
     @Test
     void constructorTest(){
         Federation fed = new Federation("Norges fotball forbund",222,"norge@fotball.no","oslo","1920");
+        assertTrue(fed.size()!= 0);
         assertTrue(fed.getFederationName() == "Norges fotball forbund");
         assertTrue(fed.getFederationNumber() == 222);
         assertTrue(fed.getFederationEmail() == "norge@fotball.no");
@@ -27,6 +30,7 @@ class FederationTest {
        Federation fed = new Federation("Norges fotball forbund",222,"norge@fotball.no","oslo","1920");
        SportsClub bomIl = new SportsClub("11",new Teamleader("HarryHole", "9a19","Harry",
                "Hole","Harry@hole.com","Team Leader" ),new Federation("BFF", 222,"bom@hjelp.no","Rygge","1521"),"Nils");
+       assertTrue(fed.getClubList.size()!=0);
        assertTrue(fed.getClubList()[0].getteamCode() == "11");
        assertTrue(fed.getClubList()[0].getTeamLeader().getUsername() == "HarryHole");
        assertTrue(fed.getClubList()[0].getFederationName() == "BFF");
@@ -36,22 +40,35 @@ class FederationTest {
 
     @Test
     void UpComingEvents(){
-        ArrayList<String> upComingEvents = new ArrayList<>();
+        Events upevent = new Events("Norges Fotball Forbund", "Ullevåll", "Norge vs Sverige", "Fotballkamp mellom Norge og Sverige",
+                new Date(2020,8,23));
+        assertTrue(upevent.getEventList.size()!=0);
+        assertTrue(upevent.getHost()=="Norges Fotball Forbund");
+        assertTrue(upevent.getLocation() == " Ullevåll");
+        assertTrue(upevent.getDescription() != "");
+        assertTrue(upevent.getDate()> LocalDate.now());
+        /*ArrayList<String> upComingEvents = new ArrayList<>();
         assertEquals(upComingEvents.size(),0);
         upComingEvents.add("Toughest2020");
         upComingEvents.add("Oslo Maraton2020");
         upComingEvents.add("Birkebeineren2020");
         assertEquals(upComingEvents.size(),3);
         upComingEvents.remove("Birkebeineren2020");
-        assertEquals(upComingEvents.size(), 2);
+        assertEquals(upComingEvents.size(), 2); */
     }
 
     @Test
     void FormerEvents(){
-        ArrayList<String> formerEvents = new ArrayList<>();
+        Events forevent = new Events("Norges Fotball Forbund", "Ullevåll", "Norge vs Sverige","Fotballkamp mellom Norge og Sverige",
+                new Date(2019,9,10));
+        assertTrue(forevent.getHost()=="Norges Fotball Forbund");
+        assertTrue(forevent.getLocation()=="Ullevåll");
+        assertTrue(forevent.getDescription() != "");
+        assertTrue(forevent.getDate()< LocalDate.now());
+       /* ArrayList<String> formerEvents = new ArrayList<>();
         formerEvents.add("Birkebeineren2018");
         formerEvents.add("Brikebeineren2017");
         formerEvents.add("Oslo Maraton2017");
-        assertEquals(formerEvents.size(),3);
+        assertEquals(formerEvents.size(),3); */
     }
 }
