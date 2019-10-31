03888064145da11e019091cf78b43d9eef7ef8f7
Tobias <tobias@icloud.com>
2019-10-31 09:04:46 +0100

    added sportsclub to athlete

--- src/no/hiof/tobiasgs/GruppeOppgaven/Model/Athlete.java
+++ src/no/hiof/tobiasgs/GruppeOppgaven/Model/Athlete.java
@@ -5,5 +5,13 @@ public class Athlete extends User {
     public Athlete(String username, String password, String firstName, String surName, String email) {
         super(username, password, firstName, surName, email);
     }
+    SportsClub sportsClub;
 
+    public SportsClub getSportsClub() {
+        return sportsClub;
+    }
+
+    public void setSportsClub(SportsClub sportsClub) {
+        this.sportsClub = sportsClub;
+    }
 }
