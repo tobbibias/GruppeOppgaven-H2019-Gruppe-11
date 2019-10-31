6620b850f2ad26464650cb2e28e8a36575930429
bomthanhtran <bomtt@hiof.no>
2019-10-31 08:53:23 +0100

    Revert "dont know what"
    This reverts commit cb562eab8bc3700f1ce9ccf68a93b5edecca4750, reversing
    changes made to c6369a68470b3cc9de21730c5b4db56f7c44e3cd.

--- Tests/no/hiof/tobiasgs/GruppeOppgaven/Model/EventTest.java
+++ Tests/no/hiof/tobiasgs/GruppeOppgaven/Model/EventTest.java
@@ -9,9 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class EventTest {
     @Test
     void eventTest() {
-        Events event = new Events(new Federation("Norges fotball forbund","norge@fotball.no", "oslo", "1920"), "Norge vs Sverige",
+        Events event = new Events(new Federation("Norges fotball forbund", 222, "norge@fotball.no", "oslo", "1920"), "Norge vs Sverige",
                 "Ullevåll", "Fotballkamp Norge vs Sverige", new Date(2020, 7, 23));
-
+        assertTrue(event.getEventList()[0].getFederationName()== "Norges Fotball Forbund");
+        assertTrue(event.getEventList()[0].getDescription()!=null);
+        assertTrue(event.getEventList()[0].getEventName()!=null);
+        assertTrue(event.getEventList()[0].getEventLocation()!=null);
+        assertTrue(event.getEventList()[0].getEventDate()!= null);
 
     }
 
--- src/no/hiof/tobiasgs/GruppeOppgaven/Model/FederationEmployee.java
+++ src/no/hiof/tobiasgs/GruppeOppgaven/Model/FederationEmployee.java
@@ -1,16 +1,10 @@
 package no.hiof.tobiasgs.GruppeOppgaven.Model;
 
-public class FederationEmployee extends User {
+public class FederationEmployee {
     private Federation federation;
     private String jobDescription;
 
-    public FederationEmployee(String username, String password, String firstName, String surName, String email, Federation federation) {
-        super(username, password, firstName, surName, email);
-        this.federation = federation;
-    }
-
-    public FederationEmployee(String username, String password, String firstName, String surName, String email, Federation federation, String jobDescription) {
-        super(username, password, firstName, surName, email);
+    public FederationEmployee(Federation federation, String jobDescription) {
         this.federation = federation;
         this.jobDescription = jobDescription;
     }
