d777800f28b2ac1da51d712c29f43fedf4459af9
bomthanhtran <bomtt@hiof.no>
2019-11-01 11:06:09 +0100

    make view class
    set up MainJavaFx and example view main. hope some feedback.

---
+++ src/no/hiof/tobiasgs/GruppeOppgaven/MainJavaFx.java
@@ -0,0 +1,18 @@
+package no.hiof.tobiasgs.GruppeOppgaven;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainJavaFx extends Application {
+
+
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
